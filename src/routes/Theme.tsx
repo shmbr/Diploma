d6593bf3cd0ecfebaa346264dd6b84976eeb0bfb
yu_shmbr <ura.shambora@gmail.com>
2024-05-18 23:57:19 +0300

    Update color component

--- src/routes/Theme.tsx
+++ src/routes/Theme.tsx
@@ -1,15 +1,15 @@
 const Theme = () => {
   const Palette = () => (
     <div>
-      <Color name="Feldgrau" hex="#3a4f41" r="58" g="79" b="65" />
-      <Color name="Citron" hex="#cacf85" r="202" g="207" b="133" />
-      <Color name="Olivine" hex="#8cba80" r="140" g="186" b="128" />
-      <Color name="Bittersweet" hex="#f25f5c" r="242" g="95" b="92" />
-      <Color name="Tomato" hex="#f95738" r="249" g="87" b="56" />
+      <Color hex="#3a4f41" />
+      <Color hex="#cacf85" />
+      <Color hex="#8cba80" />
+      <Color hex="#f25f5c" />
+      <Color hex="#f95738" />
     </div>
   );
 
-  const Color = ({ name, hex, r, g, b }) => (
+  const Color = ({ hex }: { hex: string }) => (
     <div
       style={{
         backgroundColor: hex,
